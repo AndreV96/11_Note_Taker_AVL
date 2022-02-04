333c05256ebd57fc3754f5ecc555754bff7e3543
André Vega <ventaslederer@hotmail.com>
2022-02-04 12:14:55 -0600

    Added server.js, aded TODOS for general steps of the project.

---
+++ server.js
@@ -0,0 +1,7 @@
+// TODO Create "modules" in the server.js
+// TODO Create routes in routes folder connected with an index.js
+// TODO Create middleware for requests
+// TODO Create helper functions: Connect functionality of events in the front end with the routes
+// TODO continuing last one, make sure the events are modifying correctly notes in db/
+
+// TODO deploy in heroku, make reademe
