e12c18921c8b75d226bd7887b47af648323ea5db
André Vega <ventaslederer@hotmail.com>
2022-02-04 13:52:45 -0600

    Added all basic routes, made sure the file routes work correclty and that the get notes route retrieves the data from bd.json. Also made sure the html file are openning correctly with css and js.

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
-// TODO Create "modules" in the server.js
-// TODO Create routes in routes folder connected with an index.js
+// TODO Create "modules" in the server.js DONE?
 // TODO Create middleware for requests
 // TODO Create helper functions: Connect functionality of events in the front end with the routes
 // TODO continuing last one, make sure the events are modifying correctly notes in db/
-
 // TODO deploy in heroku, make reademe
+
+const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js')
+const fs = require('fs')
+
+const PORT = 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`APP listening at http://localhost:${PORT} 🚀`)
+);
